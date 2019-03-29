73d9b1d5b156a9dd1bd73aef11b8c38a2b19e398
Olivier PEREZ <olivier@olivierperez.fr>
2019-03-29 01:17:55 +0100

    Change interpolator

--- app/src/main/java/fr/o80/prez/customcomponent/MainActivity.kt
+++ app/src/main/java/fr/o80/prez/customcomponent/MainActivity.kt
@@ -1,10 +1,10 @@
 package fr.o80.prez.customcomponent
 
 import android.animation.ValueAnimator
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.animation.AccelerateInterpolator
+import android.view.animation.DecelerateInterpolator
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
             ValueAnimator.ofFloat(0f, 1f)
                 .apply {
                     duration = 2000
-                    interpolator = AccelerateInterpolator(1.5f)
+                    interpolator = DecelerateInterpolator(0.9f)
                     addUpdateListener { animation -> pinsWheelView.progress = animation.animatedValue as Float }
                 }
                 .start()
